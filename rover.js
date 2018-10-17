7fef096e3957708f1f4cc9e4eee8601449806aa6
Chris <chrislo.7@live.com>
2018-10-17 19:41:01 +0000

    added helper to check uppercase for chai

--- rover.js
+++ rover.js
@@ -6,16 +6,50 @@ const takeInput = (filepath) => {
 }
 
 const gridSize = takeInput('input/input-data.txt')[0].split(' ').map(str => { return parseInt(str, 10)})
-const startingPosition = takeInput('input/input-data.txt')[1].split(' ')
-const movement = takeInput('input/input-data.txt')[2].split('')
+const startingPosition = takeInput('input/input-data.txt')[1].split(' ').map(str => { return str.toUpperCase() })
+const movement = takeInput('input/input-data.txt')[2].split('').map(str => { return str.toUpperCase() })
 
 console.log(movement)
 
+const leftTurn = (dir) => {
+  switch (dir) {
+    case 'N':
+      return 'W'
+      break;
+    case 'S':
+      return 'E'
+      break;
+    case 'E':
+      return 'N'
+      break;
+    case 'W':
+      return 'S'
+      break;
+  }
+}
+
+const rightTurn = (dir) => {
+  switch (dir) {
+    case 'N':
+      return 'E'
+      break;
+    case 'S':
+      return 'W'
+      break;
+    case 'E':
+      return 'S'
+      break;
+    case 'W':
+      return 'N'
+      break;
+  }
+}
 
 module.exports = {
   takeInput: takeInput,
   gridSize: gridSize,
   startingPosition: startingPosition,
-  movement: movement
+  movement: movement,
+  leftTurn: leftTurn,
+  rightTurn: rightTurn
 }
-
--- test/rover-test.js
+++ test/rover-test.js
@@ -1,40 +1,69 @@
+const chai = require('chai')
 const assert = require('chai').assert;
-const takeInput = require('../rover.js').takeInput;
-const gridSize = require('../rover.js').gridSize;
-const startingPosition = require('../rover.js').startingPosition;
+const expect = require('chai').expect;
+const assets = require('../rover.js');
+
+
+// helper to check for uppercase
+chai.Assertion.addProperty('uppercase', function() {
+  let obj = this.__flags.object;
+  new chai.Assertion(obj).to.be.a('string');
+
+  this.assert(
+      obj === obj.toUpperCase() // adapt as needed
+    , `expected ${this} to be all uppercase`   // error message when fail for normal
+    , `expected ${this} to not be all uppercase`  // error message when fail for negated
+  );
+})
 
 describe('takeInput', function() {
   it ('should take input from text file', function(){
-    let result = takeInput('input/input-data.txt')
+    let result = assets.takeInput('input/input-data.txt')
     assert.exists(result, 'input is neither `null` nor `undefined`')
   })
 
   it ('should return array for each line within txt file', function(){
-    let result = takeInput('input/input-data.txt')
+    let result = assets.takeInput('input/input-data.txt')
     assert.isArray(result, 'input is an array')
   })
 })
 
 describe('gridSize', function() {
   it ('should return an array with two elements', function(){
-    assert.exists(gridSize[0], 'first element exists')
-    assert.exists(gridSize[1], 'second element exists')
-    assert.notExists(gridSize[2], 'a third element does not exist')
+    assert.exists(assets.gridSize[0], 'first element exists')
+    assert.exists(assets.gridSize[1], 'second element exists')
+    assert.notExists(assets.gridSize[2], 'a third element does not exist')
   })
 
   it ('should return an array with two numbers', function(){
-    assert.isNumber(gridSize[0], 'element is a number')
-    assert.isNumber(gridSize[1], 'element is a number')
+    assets.gridSize.map(num => { assert.isNumber(num, 'element is a number')})
   })
 })
 
 describe('startingPosition', function() {
   it ('should return an array with three elements', function(){
-    assert.exists(startingPosition[0], 'first element exists')
-    assert.exists(startingPosition[1], 'second element exists')
-    assert.exists(startingPosition[2], 'third element exists')
-    assert.notExists(startingPosition[3], 'a fourth element does not exist')
+    assert.exists(assets.startingPosition[0], 'first element exists')
+    assert.exists(assets.startingPosition[1], 'second element exists')
+    assert.exists(assets.startingPosition[2], 'third element exists')
+    assert.notExists(assets.startingPosition[3], 'a fourth element does not exist')
+  })
+
+  it ('should change third element to uppercase', function(){
+    expect(assets.startingPosition[2]).to.be.uppercase
   })
 })
 
+describe('movement', function() {
+  it ('should change all elements to uppercase', function() {
+    assets.movement.map(str => {
+      expect(str).to.be.uppercase
+    })
+  })
+})
+
+// describe('leftTurn', function() {
+//   it ('should show correct cardinal directions', function(){
+//     assert.equals(assets.leftTurn('W'), )
+//   })
+// })
 
