018b90e9b391b69c6acb199ee61e3c6c781910dd
Chris <chrislo.7@live.com>
2018-10-17 18:39:32 +0000

    tests for grid size, starting position and movement

--- rover.js
+++ rover.js
@@ -5,7 +5,17 @@ const takeInput = (filepath) => {
   return fs.readFileSync(filepath).toString().split("\n")
 }
 
+const gridSize = takeInput('input/input-data.txt')[0].split(' ').map(str => { return parseInt(str, 10)})
+const startingPosition = takeInput('input/input-data.txt')[1].split(' ')
+const movement = takeInput('input/input-data.txt')[2].split('')
+
+console.log(movement)
+
+
 module.exports = {
-  takeInput: takeInput
+  takeInput: takeInput,
+  gridSize: gridSize,
+  startingPosition: startingPosition,
+  movement: movement
 }
 
--- test/rover-test.js
+++ test/rover-test.js
@@ -1,5 +1,7 @@
 const assert = require('chai').assert;
 const takeInput = require('../rover.js').takeInput;
+const gridSize = require('../rover.js').gridSize;
+const startingPosition = require('../rover.js').startingPosition;
 
 describe('takeInput', function() {
   it ('should take input from text file', function(){
@@ -12,3 +14,27 @@ describe('takeInput', function() {
     assert.isArray(result, 'input is an array')
   })
 })
+
+describe('gridSize', function() {
+  it ('should return an array with two elements', function(){
+    assert.exists(gridSize[0], 'first element exists')
+    assert.exists(gridSize[1], 'second element exists')
+    assert.notExists(gridSize[2], 'a third element does not exist')
+  })
+
+  it ('should return an array with two numbers', function(){
+    assert.isNumber(gridSize[0], 'element is a number')
+    assert.isNumber(gridSize[1], 'element is a number')
+  })
+})
+
+describe('startingPosition', function() {
+  it ('should return an array with three elements', function(){
+    assert.exists(startingPosition[0], 'first element exists')
+    assert.exists(startingPosition[1], 'second element exists')
+    assert.exists(startingPosition[2], 'third element exists')
+    assert.notExists(startingPosition[3], 'a fourth element does not exist')
+  })
+})
+
+
